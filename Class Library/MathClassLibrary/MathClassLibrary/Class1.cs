d10048f98603d84d0021fe5b2b58551102b6b866
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-11-22 11:11:59 +0530

    Add files via upload

---
+++ Class Library/MathClassLibrary/MathClassLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace MathClassLibrary
+{
+        public class MathClass//no main method is included in Class Library
+        {
+            public int add(int no1, int no2)
+            {
+                return no1 + no2;
+            }
+            public int sub(int no1, int no2)
+            {
+                return no1 - no2;
+            }
+            public int mul(int no1, int no2)
+            {
+                return no1 * no2;
+            }
+            public int div(int no1, int no2)
+            {
+                return no1 / no2;
+            }
+        }
+    }
