0bdaf483ff9ae520dcda248155dffdf8bdc69f23
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-11-22 23:11:39 +0530

    Add files via upload

---
+++ Benchmark/Assignment 4/DirectoryInfo/DirectoryInfo/Program.cs
@@ -0,0 +1,45 @@
+﻿//Use DirectoryInfo and FileInfo class and print details of directories and files available in that directory
+//IOExample
+
+using System.IO;
+
+namespace IOExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Directory.CreateDirectory("path");
+                DirectoryInfo di = new DirectoryInfo("C:\\Users\\Manasi\\source\\repos");
+                if (di.Exists)
+                {
+                    Console.WriteLine(di.FullName);
+                    Console.WriteLine(di.CreationTime);
+                    Console.WriteLine(di.Parent);
+                    Console.WriteLine(di.LastWriteTime);
+                    FileInfo[] fi = di.GetFiles();
+                    foreach (FileInfo f in fi)
+                    {
+                        Console.WriteLine(f.FullName);
+                        Console.WriteLine(f.Extension);
+                        Console.WriteLine(f.CreationTime);
+                        Console.WriteLine(f.Length);
+                        Console.WriteLine("_____________________");
+                    }
+                }
+                else
+                {
+                    di.Create();
+                    Console.WriteLine("Directory Created");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: "+ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Benchmark/Assignment 4/GenericDemo/GenericDemo/Program.cs
@@ -0,0 +1,65 @@
+﻿//Write a program in C# to accept list of employees and print list of employees.(using generic collection)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace GenericDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Generic HashSet ");
+            HashSet<Employee> employees = new HashSet<Employee> {
+                {
+                    new Employee {
+                        Emp_Id = 101, Emp_name = "John", Dept_name = "HR"
+                    }
+                },
+                {
+                    new Employee {
+                        Emp_Id = 102, Emp_name = "Smith", Dept_name = "IT"
+                    }
+                },
+                {
+                    new Employee {
+                        Emp_Id = 103, Emp_name = "Robert", Dept_name = "Accounts"
+                    }
+                },
+                {
+                    new Employee {
+                        Emp_Id = 104, Emp_name = "Scott", Dept_name = "IT"
+                    }
+                }
+            };
+            Console.WriteLine("{0,-6}{1,10}{2,-8}", "Emp_Id", "Emp_name", "Dept_name");
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("{0,-8}{1,-10}{2,5}", employee.Emp_Id, employee.Emp_name, employee.Dept_name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+    public class Employee
+    {
+        public int Emp_Id
+        {
+            get;
+            set;
+        }
+        public string Emp_name
+        {
+            get;
+            set;
+        }
+        public string Dept_name
+        {
+            get;
+            set;
+        }
+    }
+}
