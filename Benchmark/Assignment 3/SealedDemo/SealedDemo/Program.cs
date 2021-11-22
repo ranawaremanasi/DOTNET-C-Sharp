b422e2b421125f1d45031aa533b673d4e39437f4
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-11-22 11:49:01 +0530

    Add files via upload

---
+++ Benchmark/Assignment 3/SealedDemo/SealedDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace SealedDemo
+{
+    class Parent
+    {
+        public virtual void Demo()
+        {
+            Console.WriteLine("Parent Demo");
+        }
+    }
+    class Child : Parent
+    {
+        public sealed override void Demo()
+        {
+            Console.WriteLine("Child Demo");
+        }
+    }
+    class GrandChild : Child
+    {
+        //public override void Demo()
+        //{
+        //    Console.WriteLine("GrandChild Demo");
+        //}
+        
+    }
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Parent p=new Parent();
+            p.Demo();
+            Console.WriteLine("this is parent");
+            Console.ReadKey();
+    }
+}
+}
