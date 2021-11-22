3122e5cd19c8a1365510a05a741a582facee5675
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-11-22 11:47:22 +0530

    Add files via upload

---
+++ Benchmark/Assignment 2/enum/enum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    class enumDemo
+    {
+        public enum Days { SUN = 1, MON, TUE, WED, THU, FRI, SAT}
+        static void Main(string[] args)
+        {
+            //Days d;
+            //d = Days.MON;
+            //int monday = (int)Days.MON;
+            //Console.WriteLine(monday);
+
+            enumDemo e = new enumDemo();
+            e.PrintDay(Days.MON);
+            
+        }
+
+        public void PrintDay(Days d)
+        {
+            Console.WriteLine(d);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Benchmark/Assignment 2/paramarr/paramarr/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Write Average function to verify working param array. Pass different no of parameters and verify
+
+internal class Program
+{
+    public int findAvg(params int[] arr)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum = sum + arr[i];
+        }
+        int avg;
+        avg = sum / arr.Length;
+        return avg;
+    }
+    static void Main(string[] args)
+    {
+        Program p = new Program();
+        int x = p.findAvg(1, 2, 3, 4, 5);
+        Console.WriteLine("Average : " + x);
+        Console.ReadKey();
+    }
+}
