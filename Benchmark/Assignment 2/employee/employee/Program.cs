e9cff0d4ff58683841ed602d91a09251dabbf7c6
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-11-22 11:45:11 +0530

    Add files via upload

---
+++ Benchmark/Assignment 2/employee/employee/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+ 
+namespace Demo
+{
+    public class Program
+    {
+        static void Main(string [] args)
+        {
+            Employee e1 = new Employee();
+            e1.PrintData();
+
+            Employee e2= new Employee("Manasi","ABC");
+            e2.PrintData();
+            Console.ReadKey();
+        }
+    }
+
+    class Employee
+    {
+        static int srno;
+        int empno;
+        string name, designation;
+
+        public Employee()
+        {
+            empno = ++srno;
+            name = "Asha";
+            designation = "Manager";
+        }
+
+        public Employee (string name,string designation)
+        {
+            this.empno = ++srno;
+            this.name = name;
+            this.designation = designation; 
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("Employee no =" + empno + "Name =" + name + "Designation =" + designation);
+        }
+    }
+}
