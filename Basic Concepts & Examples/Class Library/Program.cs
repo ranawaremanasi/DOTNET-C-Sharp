0264b580daad1b701115b1a8ee4004cc9bb3c9cc
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-11-25 22:52:35 +0530

    Add files via upload

---
+++ Basic Concepts & Examples/Class Library/Program.cs
@@ -0,0 +1,16 @@
+﻿using MathClassLibrary;
+namespace ClassLibUser
+{
+    class Pragram
+    {
+        static void Main(string[] args)
+        {
+            MathClassLibrary.MathClass obj = new MathClassLibrary.MathClass();
+            Console.WriteLine(obj.add(4, 5));
+            Console.WriteLine(obj.sub(4, 5));
+            Console.WriteLine(obj.mul(4, 5));
+            Console.WriteLine(obj.div(4, 5));
+            Console.ReadKey();
+        }
+    }
+}
