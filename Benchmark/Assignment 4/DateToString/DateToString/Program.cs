a976e1dc215e420be9906456341e4269ca35cfe3
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-11-26 14:00:41 +0530

    Add files via upload

---
+++ Benchmark/Assignment 4/DateToString/DateToString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverideFunction
+{
+    public class Date
+    {
+        public int hrs { get; set; }
+        public int mins { get; set; }
+        public int secs { get; set; }
+
+        public override string ToString()
+        {
+            return this.hrs + ": " + this.mins + " :" + this.secs;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date d = new Date() { hrs= 1, mins = 46, secs = 32 };
+
+            Console.WriteLine(d.ToString());
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
