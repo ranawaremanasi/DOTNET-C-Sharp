18adc29dac031f9119f5f4d9d74560c690b637e8
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-11-29 08:43:17 +0530

    Add files via upload

---
+++ Benchmark/Assignment 4/TemperatureCalculator/TemperatureCalculator/Class1.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TemperatureCalculator
+{
+    public class Class1
+    {
+       
+            public float FehernheitToCelsius(float Fehrenheit)
+            {
+                return (Fehrenheit - 32) * 5 / 9;
+            }
+        }
+    
+}
---
+++ Benchmark/Assignment 4/TemperatureExample/TemperatureExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TemperatureLibrary;
+using TemperatureCalculator;
+
+
+namespace TemperatureLibrary
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperatureCalculator.Class1 temp1 = new TemperatureCalculator.Class1();
+            Console.WriteLine(temp1.FehernheitToCelsius(76));
+            Console.ReadKey();
+        }
+    }
+}
