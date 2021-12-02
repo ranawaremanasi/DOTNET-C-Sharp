21f66f608bd34995e86d6dbc07bddf7c51d4a056
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-12-02 21:07:21 +0530

    Create DepartmentCrudMVC.sql

---
+++ Benchmark/ASP.NET MVC CRUD/DepartmentCrudMVC.sql
@@ -0,0 +1,10 @@
+use EmployeeDB
+exec dbo.Get_Department
+
+exec dbo.GetDeptnousingdno 1
+
+exec dbo.InsertDepartment 11,'Adam','Mumbai','Vijaya'
+
+exec dbo.UpdateDepartment 11,'Edward','Havana','Jaya'
+
+exec dbo.DeleteDepartment 11
