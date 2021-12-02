91019503fa765f159cf959e10d3bc59cd63747c3
ranawaremanasi <74227479+ranawaremanasi@users.noreply.github.com>
2021-12-02 21:39:35 +0530

    Add files via upload

---
+++ Benchmark/Models/DepartmentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class DepartmentModel
+    {
+
+        public int deptno { get; set; }
+        public string dname { get; set; }
+        public string location { get; set; }
+        public string dhead { get; set; }
+    }
+}
